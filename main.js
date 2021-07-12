55d588872131815cdc63dee8ccb24cbb91e9165c
Inna-Ka <kasperovicz.inna@gmail.com>
2021-07-12 19:51:17 +0300

    work with arrays

--- main.js
+++ main.js
@@ -20,3 +20,24 @@ if (test > 10) {
 }
 // console will show "true"
 console.log(test);
+
+let result = 1;
+let array = [2, 3, 5, 8];
+for (let i = 0; i < array.length; i++) {
+  result *= array[i]
+  }
+  console.log (result);
+
+let array2 = [2, 5, 8, 15, 0, 6, 20, 3];
+for (let i = 0; i < array2.length; i++) {
+  if (array2[i] > 5 && array2[i] < 10){
+  console.log (array2[i]);
+  }
+};
+
+let array3= [2, 5, 8, 15, 0, 6, 20, 3];
+for (let i = 0; i < array3.length; i++) {
+  if (array3[i] % 2 === 0) {
+  console.log (array3[i]);
+  }
+};

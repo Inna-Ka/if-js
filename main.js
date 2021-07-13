1a9c2f4b25b5b37c0e0f9bb547e27dae188429ce
Inna-Ka <kasperovicz.inna@gmail.com>
2021-07-13 12:28:02 +0300

    fixed tab

--- main.js
+++ main.js
@@ -22,18 +22,18 @@ let array = [2, 3, 5, 8];
 for (let i = 0; i < array.length; i++) {
   result *= array[i]
 }
-  console.log (result);
+console.log (result);
 
 let array2 = [2, 5, 8, 15, 0, 6, 20, 3];
 for (let i = 0; i < array2.length; i++) {
   if (array2[i] > 5 && array2[i] < 10){
-  console.log (array2[i]);
+    console.log (array2[i]);
   }
 }
 
 let array3= [2, 5, 8, 15, 0, 6, 20, 3];
 for (let i = 0; i < array3.length; i++) {
   if (array3[i] % 2 === 0) {
-  console.log (array3[i]);
+    console.log (array3[i]);
   }
 }

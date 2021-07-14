de70183dbbafba11334c9b8dd85fd9c94d51ea0f
Inna-Ka <74614917+Inna-Ka@users.noreply.github.com>
2021-07-14 09:49:24 +0300

    Lesson 2 (#2)
    * added files (#1)
    
    * added files
    
    * changed file .gitignore
    
    * installed react
    
    * changed .gitignore
    
    * changed .gitignore
    
    * deleted modules
    
    * fixed gitignore
    
    * work with variables an primitives
    
    * work with arrays
    
    * fixed spaces and bool
    
    * fixed tab
    
    * fixed space

---
+++ main.js
@@ -0,0 +1,39 @@
+let user = 'John doe';
+console.log(user);
+let student = 'Inna';
+console.log(student);
+user = student;
+// user become = Inna
+console.log(user);
+let test = 1;
+test++;
+test += '1';
+// test = 21 and will string
+console.log(test);
+test -= 1;
+// test=20 and will number
+console.log(test);
+test = !!0;
+// console will show "false"
+console.log(test);
+
+let result = 1;
+let array = [2, 3, 5, 8];
+for (let i = 0; i < array.length; i++) {
+  result *= array[i]
+}
+console.log (result);
+
+let array2 = [2, 5, 8, 15, 0, 6, 20, 3];
+for (let i = 0; i < array2.length; i++) {
+  if (array2[i] > 5 && array2[i] < 10) {
+    console.log (array2[i]);
+  }
+}
+
+let array3= [2, 5, 8, 15, 0, 6, 20, 3];
+for (let i = 0; i < array3.length; i++) {
+  if (array3[i] % 2 === 0) {
+    console.log (array3[i]);
+  }
+}
